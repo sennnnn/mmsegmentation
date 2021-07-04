641e7ca6b5ee17917c173bc8341181ba3068c148
sennnnn <201730271412@mail.scut.edu.cn>
2021-07-04 17:14:55 +0800

    Modify config name.

--- configs/dummy/upernet_r50_512x1024_40k_dummy_cityscapes.py
+++ configs/dummy/upernet_r50_512x1024_40k_dummy_cityscapes.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8.py',
+    '../_base_/models/upernet_r50.py',
     '../_base_/datasets/dummy_cityscapes.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_40k.py'
 ]
