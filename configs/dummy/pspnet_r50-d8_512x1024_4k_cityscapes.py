ed93177a99811028a4271b295cc810a2d8b1469c
sennnnn <201730271412@mail.scut.edu.cn>
2021-07-05 12:39:35 +0800

    Storage dummy dataset speed test.

--- configs/dummy/pspnet_r50-d8_512x1024_4k_cityscapes.py
+++ configs/dummy/pspnet_r50-d8_512x1024_4k_cityscapes.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/deeplabv3_r50-d8.py',
+    '../_base_/models/pspnet_r50-d8.py',
     '../_base_/datasets/dummy_cityscapes.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py'
+    '../_base_/schedules/schedule_4k.py'
 ]
